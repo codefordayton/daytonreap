aa0169280ceee4ec13ac955e28d55d22acc73a63
David Best <david.best@gsa.gov>
2014-10-07 22:05:43 -0400

    Initial commit, output of yeoman angular generator 0.9.8

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name daytonreapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the daytonreapApp
+ */
+angular.module('daytonreapApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
