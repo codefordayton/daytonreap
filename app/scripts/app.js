76f98651b85798e4034f3fd93e81cc2ed77bc7f7
David Best <david.best@gsa.gov>
2014-10-11 20:51:19 -0400

    Adding a basic page with a map.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,4 @@
  * Main module of the application.
  */
 angular
-  .module('daytonreapApp', []);
+  .module('daytonreapApp', ['leaflet-directive']);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,13 @@
  */
 angular.module('daytonreapApp')
   .controller('MainCtrl', function ($scope) {
+    angular.extend($scope, {
+      dayton: {
+        lat: 39.758948,
+        lng: -84.191607,
+        zoom: 10 
+      }
+    });
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
